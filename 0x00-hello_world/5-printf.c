d2c62109f9f124b5b34689f748a4adc28190d7dc
Chinwendu Evans <tisevadon085@gmail.com>
2022-09-08 17:45:34 -0700

    Printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-int main()
-{
-  printf("with proper grammar, but the outcome is a piece of art,\n");
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 
-  return (0);
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
