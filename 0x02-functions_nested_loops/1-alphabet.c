bada57c9f8182943b3da5aecf318c5b39e538f70
Chinwendu Evans <tisevadon085@gmail.com>
2022-09-13 16:17:45 -0700

    Printing lowercase alphabets 10 times

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,11 @@
 
 void print_alphabet(void)
 {
-	int ch;
+	int x;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
-		_putchar('ch');
+		_putchar(x);
 	}
 	_putchar('\n');
-	return (0);
 }
