578325a5e729ee082821c9f02648b8ad06c88793
Chinwendu Evans <tisevadon085@gmail.com>
2022-09-30 19:07:36 -0700

    Function that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints its name
+ * @argc: integer type
+ * @argv: pointer to char type
+ * Return: return 0 if successful
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Projects on C agrc and argv
